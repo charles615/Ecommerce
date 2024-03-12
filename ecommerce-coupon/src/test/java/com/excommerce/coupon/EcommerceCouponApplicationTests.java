413435ac60e6e84852a687e9abc348a2601c548c
Charles <22453341liu@gmail.com>
2024-03-12 20:30:21 +0800

    Build the initial project structure

---
+++ ecommerce-coupon/src/test/java/com/excommerce/coupon/EcommerceCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excommerce.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ecommerce-ware/src/test/java/com/ecommerce/ware/EcommerceWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
